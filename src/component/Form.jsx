217de3989c2ea1698f697216429f1d11ded7d408
Onifel <feloniral@gmail.com>
2023-07-27 19:05:31 -0700

    Modifier Form file

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import logo from '../logo-mndpt.jpeg';
+import logo from '../public/logo-mndpt.jpg';
 
 const Form = () => {
     return (
